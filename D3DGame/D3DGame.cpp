89fb59e5ad3af094f4b0db202b71dcbdb5a69d7a
Ggoom94 <cbg1541@gmail.com>
2018-05-24 19:24:40 +0900

    init, clear, render

--- D3DGame/D3DGame.cpp
+++ D3DGame/D3DGame.cpp
@@ -2,12 +2,16 @@
 #include<d3d9.h>
 
 LRESULT CALLBACK MsgProc(HWND, UINT, WPARAM, LPARAM);
+HRESULT InitD3D(HWND hWnd);
+VOID CleanUP();
+VOID Render();
+
 HINSTANCE g_hInst;
 HWND hWndMain;
 LPCTSTR lpszClass = TEXT("D3D Game");
 
-LPDIRECT3D9 g_pD3D = NULL;					//Direct3D 객체
-LPDIRECT3DDEVICE9 q_pd3dDevice = NULL;		//랜더링 장치(비디오 카드)
+LPDIRECT3D9 g_pD3D = NULL;					// Direct3D 객체
+LPDIRECT3DDEVICE9 g_pd3dDevice = NULL;		// 랜더링 장치(비디오 카드)
 
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
@@ -17,16 +21,24 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmd
 
 	WNDCLASSEX WndClass = { sizeof(WNDCLASSEX), CS_CLASSDC, MsgProc, 0L, 0L, GetModuleHandle(NULL), NULL,NULL,NULL,NULL, lpszClass, NULL };
 	RegisterClassEx(&WndClass);
-
 	hWnd = CreateWindow(lpszClass, TEXT("D3D Game Program"), WS_OVERLAPPEDWINDOW, 100, 100, 300, 300, GetDesktopWindow(), NULL, WndClass.hInstance, NULL);
 
-	ShowWindow(hWnd, nCmdShow);
-	UpdateWindow(hWnd);
-	while (GetMessage(&Message, NULL, 0, 0))
-	{
-		TranslateMessage(&Message);
-		DispatchMessage(&Message);
+
+	// Direct3D 초기화에 성공하면 진행하고, 실패하면 종료한다.
+	if (SUCCEEDED(InitD3D(hWnd))) {
+
+		// 윈도우 출력
+		ShowWindow(hWnd, nCmdShow);
+		UpdateWindow(hWnd);
+
+		// 메시지 루프 시작하기
+		while (GetMessage(&Message, NULL, 0, 0))
+		{
+			TranslateMessage(&Message);
+			DispatchMessage(&Message);
+		}
 	}
+
 	UnregisterClass(lpszClass, WndClass.hInstance);
 	return 0;
 }
@@ -40,9 +52,11 @@ LRESULT WINAPI MsgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 		hWndMain = hWnd;
 		return 0;
 	case WM_PAINT:
+		Render();			// 화면 출력을 담당하는 랜더링 함수 호출
 		ValidateRect(hWnd, NULL);
 		return 0;
 	case WM_DESTROY:
+		CleanUP();			// 프로그램 종룔 시 객체 해제를 위하여 호출함;
 		PostQuitMessage(0);
 		return 0;
 	}
@@ -56,25 +70,67 @@ LRESULT WINAPI MsgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 //	기능 : Direct3D 초기화
 //-------------------------------------------------------
 
-HRESULT Init(HWND hWnd) {
+HRESULT InitD3D(HWND hWnd) {
 	//Direct3D 객체 생성
 	if (NULL == (g_pD3D = Direct3DCreate9(D3D_SDK_VERSION)))
 		return E_FAIL;
 
-	//장치 생성용 데이터 준비
-	D3DPRESENT_PARAMETERS d3dpp;				//장치 생성용 정보 구조체 변수 선언
-	ZeroMemory(&d3dpp, sizeof(d3dpp));			//변수 클리어
-	d3dpp.Windowed = TRUE;						//윈도우 모드로 실행 설정
-	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;	//스왑 방법 설정
 
-	//백버퍼 포맷은 현재 데스크탑 형식으로 설정
+	// 장치 생성용 데이터 준비
+	D3DPRESENT_PARAMETERS d3dpp;				// 장치 생성용 정보 구조체 변수 선언
+	ZeroMemory(&d3dpp, sizeof(d3dpp));			// 변수 클리어
+	d3dpp.Windowed = true;						// 윈도우 모드로 실행 설정
+	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;	// 스왑 방법 설정
+
+	// 백버퍼 포맷은 현재 데스크탑 형식으로 설정
 	d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
 
-	//D3D객체의 장치 생성 함수 호출 (디폴트 비디오카드 사용 HAL 사용, 소프트웨어 버텍스 처리사용 지정)
-	if (FAILED(g_pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &q_pd3dDevice))) {
+	// D3D객체의 장치 생성 함수 호출 (디폴트 비디오카드 사용 HAL 사용, 소프트웨어 버텍스 처리사용 지정)
+	if (FAILED(g_pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &g_pd3dDevice))) {
 		return E_FAIL;
 	}
 
-	//이제 장치가 정상적으로 생성되었음.
+	// 이제 장치가 정상적으로 생성되었음.
 	return S_OK;
+}
+
+//-------------------------------------------------------
+//	이름 : Cleanup()
+//	기능 : 초기화되었던 모든 객체들을 해제한다.
+//-------------------------------------------------------
+
+VOID CleanUP() {
+	if (g_pd3dDevice != NULL)		// 장치 객체 해제
+		g_pd3dDevice->Release();
+
+	if (g_pd3dDevice != NULL)
+		g_pD3D->Release();			// D3D 객체 해제
+}
+
+//-------------------------------------------------------
+//	이름 : Render()
+//	기능 : 화면을 그린다.
+//-------------------------------------------------------
+
+VOID Render() {
+
+	// if문에서 NULL 값을 왼쪽에 두는 이유는 프로그래머 실수를 방지 하기 위해!!
+	// ex) g_pd3dDevice = NULL <<< 같은 실수가 발생 할 수도 있기때문에 아래와 같이 하는 습관.
+
+	if (NULL == g_pd3dDevice)		// 장치 객체가 생성되지 않았으면 리턴
+		return;
+
+	// 백버퍼를 지정된 색상으로 지운다. (여기에는 청색으로 지움)
+	g_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
+
+	// 화면 그리기 시작
+	if (SUCCEEDED(g_pd3dDevice->BeginScene())) {
+		//이 위치에 3D 객체들을 추가 하게되는 부분
+
+		//화면 그리기 끝
+		g_pd3dDevice->EndScene();
+	}
+
+	// 백퍼퍼의 내용을 화면으로 보낸다.
+	g_pd3dDevice->Present(NULL, NULL, NULL, NULL);
 }
