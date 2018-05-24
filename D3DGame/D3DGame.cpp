efa2be5c982aae1b416e4fefbb48b18f97c92327
Ggoom94 <cbg1541@gmail.com>
2018-05-24 20:21:14 +0900

    버텍스 버퍼 추가

--- D3DGame/D3DGame.cpp
+++ D3DGame/D3DGame.cpp
@@ -1,8 +1,9 @@
 #include<Windows.h>
-#include<d3d9.h>
+#include<d3dx9.h>
 
 LRESULT CALLBACK MsgProc(HWND, UINT, WPARAM, LPARAM);
 HRESULT InitD3D(HWND hWnd);
+HRESULT InitGeometry();
 VOID CleanUP();
 VOID Render();
 
@@ -12,6 +13,18 @@ LPCTSTR lpszClass = TEXT("D3D Game");
 
 LPDIRECT3D9 g_pD3D = NULL;					// Direct3D 객체
 LPDIRECT3DDEVICE9 g_pd3dDevice = NULL;		// 랜더링 장치(비디오 카드)
+LPDIRECT3DVERTEXBUFFER9 g_pVB = NULL;		// 버텍스 버퍼
+
+// 커스텀 버텍스 타입 구조체
+struct CUSTOMVERTEX
+{
+	FLOAT x, y, z;		// 3D 좌표값
+	DWORD color;		// 버텍스 색상
+};
+
+// 커스텀 버텍스의 구조를 표시하기 위한 FVF (Flexible Vertex Format) 값
+// D3DFVF_XYZ (3D 월드 좌표) 와 D3DFVF_DIFFUSE (점의 색상) 특성을 가지도록.
+#define D3DFVF_CUSTOMVERTEX (D3DFVF_XYZ | D3DFVF_DIFFUSE)
 
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
@@ -24,8 +37,8 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmd
 	hWnd = CreateWindow(lpszClass, TEXT("D3D Game Program"), WS_OVERLAPPEDWINDOW, 100, 100, 300, 300, GetDesktopWindow(), NULL, WndClass.hInstance, NULL);
 
 
-	// Direct3D 초기화에 성공하면 진행하고, 실패하면 종료한다.
-	if (SUCCEEDED(InitD3D(hWnd))) {
+	// Direct3D 초기화에 성공하고, 버텍스 버퍼 생성에 성공하면 진행, 실패하면 종료한다.
+	if (SUCCEEDED(InitD3D(hWnd)) && SUCCEEDED(InitGeometry())) {
 
 		// 윈도우 출력
 		ShowWindow(hWnd, nCmdShow);
@@ -94,6 +107,41 @@ HRESULT InitD3D(HWND hWnd) {
 	return S_OK;
 }
 
+
+//-------------------------------------------------------
+//	이름 : InitGeometry()
+//	기능 : 버텍스 버퍼를 생성한 후 버텍스로 채운다.
+//-------------------------------------------------------
+
+HRESULT InitGeometry() {
+	// 삼각형을 그리기 위하여 3 점에 대한 임시 배열을 만든다.
+	CUSTOMVERTEX vertices[] = {
+		{ 0.0f, 100.0f, 0.0f, 0xffff0000, },		// 버텍스의 위치와 색상
+		{100.0f, -100.0f, 0.0f, 0xff00ffff, },
+		{-100.0f, -100.0f, 0.0f, 0xff00ff00, },
+
+		// 0xffff0000 : 0x	ff (투명도)	00 (Red)	ff (Green)	ff (Blue)
+	};
+
+	// 버텍스 버퍼를 생성한다. 
+	// 각 버텍스의 포맷은 D3DFVF_CUSTOMVERTEX 라는 것도 전달
+	if (FAILED(g_pd3dDevice->CreateVertexBuffer(3 * sizeof(CUSTOMVERTEX), 0, D3DFVF_CUSTOMVERTEX, D3DPOOL_DEFAULT, &g_pVB, NULL))) {
+		return E_FAIL;
+	}
+
+	// 버텍스 버퍼에 락을 건 후 버텍스를 넣는다.
+	VOID* pVertices;
+	if (FAILED(g_pVB->Lock(0, sizeof(vertices), (void**)&pVertices, 0)))
+		return E_FAIL;
+	
+	memcpy(pVertices, vertices, sizeof(vertices));
+	g_pVB->Unlock();
+
+	return S_OK;
+	
+}
+
+
 //-------------------------------------------------------
 //	이름 : Cleanup()
 //	기능 : 초기화되었던 모든 객체들을 해제한다.
@@ -125,7 +173,15 @@ VOID Render() {
 
 	// 화면 그리기 시작
 	if (SUCCEEDED(g_pd3dDevice->BeginScene())) {
-		//이 위치에 3D 객체들을 추가 하게되는 부분
+		//// 버텍스 출력 부분
+		// 1. 버텍스 버퍼 지정
+		g_pd3dDevice->SetStreamSource(0, g_pVB, 0, sizeof(CUSTOMVERTEX));
+
+		// 2. 버텍스 포멧 지정
+		g_pd3dDevice->SetFVF(D3DFVF_CUSTOMVERTEX);
+
+		// 3. 버텍스를 이용하여 그릴 도형 지정
+		g_pd3dDevice->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 1);
 
 		//화면 그리기 끝
 		g_pd3dDevice->EndScene();
